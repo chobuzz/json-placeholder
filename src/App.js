d80d18e899c73709b697d8ef3ee1cbf9600935bb
chobuzz <chobuzz23@gmail.com>
2023-10-02 20:30:30 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const API_URL = "https://jsonplaceholder.typicode.com/";
+  const [reqType, setReqType] = useState("users");
+  const [items, setItems] = useState([]);
+  return <div className="App"></div>;
+}
+
+export default App;
